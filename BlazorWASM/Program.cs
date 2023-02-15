5a7cefd0a6d3174400f25e02135fbf265596852b
Bogdan0002 <fellyx1@gmail.com>
2023-02-15 12:08:42 +0100

    updates

---
+++ BlazorWASM/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWASM;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync();
+
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+builder.Services.AddScoped(
+    sp => 
+        new HttpClient { 
+            BaseAddress = new Uri("https://localhost:7093") 
+        }
+);
+
+builder.Services.AddScoped<IUserService, UserHttpClient>();
